0ce8df7db595477d28e6dbe534bd0a2f0fdd6091
Calvin French-Owen <calvin@calv.info>
2014-07-23 15:40:59 -0700

    updating error message/counter

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ var debug = require('debug')('s-blocked-stats');
  */
 
 module.exports = function(log, counters){
+  var msg = 'Event loop blocked';
   blocked(function(ms){
-    counters.get('blocked').update(ms);
-    if (ms > 10000) log.error('blocked', { ms: ms })
-    else if (ms > 1000) log.warn('blocked', { ms: ms })
-    else if (ms > 100) debug('blocked for %sms', ms | 0);
+    counters.get(msg).update(ms);
+    if (ms > 10000) log.error(msg, { ms: ms })
+    else if (ms > 1000) log.warn(msg, { ms: ms })
+    else if (ms > 100) debug('Event loop blocked for %sms', ms | 0);
   });
 };
